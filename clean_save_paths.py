a2d101cf484683b7bd56e080a6373cc54a30f0b8
saleml <salemlahlou9@gmail.com>
2022-09-20 13:41:03 -0400

    better cleaning

--- clean_save_paths.py
+++ clean_save_paths.py
@@ -12,6 +12,23 @@ def remove_empty_folders(path_abs):
     print(f"Removed {s} empty folders")
 
 
+def clean_failed_configs_file(path):
+    done_configs_file = os.path.join("configs", f"{path}_done_configs")
+    failed_configs_file = os.path.join("configs", f"{path}_failed_configs")
+    with open(done_configs_file, "r+") as f:
+        done_configs = f.read().splitlines()
+    with open(failed_configs_file, "r+") as f1:
+        failed_configs = f1.read().splitlines()
+    print(f"Done configs: {len(done_configs)}")
+    print(f"Failed configs: {len(failed_configs)}")
+    new_failed_configs = set(failed_configs) - set(done_configs)
+    new_failed_configs = list(new_failed_configs)
+    print(f"New failed configs: {len(new_failed_configs)}")
+    failed_configs_file_2 = os.path.join("configs", f"{path}_failed_configs")
+    with open(failed_configs_file_2, "w+") as f2:
+        f2.write("\n".join(new_failed_configs))
+
+
 if __name__ == "__main__":
     from argparse import ArgumentParser
 
@@ -21,4 +38,5 @@ if __name__ == "__main__":
 
     path = os.path.join(os.environ["SCRATCH_PATH"], args.path, "models")
     remove_empty_folders(path)
+    clean_failed_configs_file(args.path)
     print("OK")
